63da1fb2455a7ef64c469b1b0723d8da5a19630b
Shubham8037 <nuser8037@gmail.com>
2020-08-01 09:07:46 -0400

    Upload working

--- server/server.js
+++ server/server.js
@@ -9,30 +9,30 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 
 app.get('/api/images', async (req, res) => {
-    const { resources } = await cloudinary.search
-        .expression('folder:dev_setups')
-        .sort_by('public_id', 'desc')
-        .max_results(30)
-        .execute();
+  const { resources } = await cloudinary.search
+    .expression('folder:dev_setups')
+    .sort_by('public_id', 'desc')
+    .max_results(30)
+    .execute();
 
-    const publicIds = resources.map((file) => file.public_id);
-    res.send(publicIds);
+  const publicIds = resources.map(file => file.public_id);
+  res.send(publicIds);
 });
 app.post('/api/upload', async (req, res) => {
-    try {
-        const fileStr = req.body.data;
-        const uploadResponse = await cloudinary.uploader.upload(fileStr, {
-            upload_preset: 'dev_setups',
-        });
-        console.log(uploadResponse);
-        res.json({ msg: 'yaya' });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ err: 'Something went wrong' });
-    }
+  try {
+    const fileStr = req.body.data;
+    const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: 'image-upload',
+    });
+    console.log(uploadResponse);
+    res.json({ msg: 'yaya' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: 'Something went wrong' });
+  }
 });
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log('listening on 3001');
+  console.log('listening on 3001');
 });
